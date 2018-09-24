d23d994563b4d686bb9f10f8b4a24847bf5f3dd5
andreiyasinski <andrewtopol856@gmail.com>
2018-09-24 16:00:19 +0300

    :tada:

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+
+  var compareFunc = (a,b) => a - b;
+
+  var x1 = 0;
+  var x2 = 0;
+
+  var equation = equation;
+
+  var arr = [];
+  var newArr = [];
+
+  arr = equation.split(' * ');
+
+  var a = +arr[0];
+  var [, simbol1, value1] = arr[1].split(' ');
+  var b = +(simbol1 + '' + value1);
+  var [, simbol2, value2] = arr[2].split(' ');
+  var c = +(simbol2 + '' + value2);
+
+  var d = b*b - 4*a*c;
+
+  if( d >= 0 ) {
+    x1 = Math.round((-b + Math.sqrt(d)) / (2 * a));
+    x2 = Math.round((-b - Math.sqrt(d)) / (2 * a));
+
+    newArr.push(x1);
+    newArr.push(x2);
+
+    newArr.sort(compareFunc);
+    return newArr;
+  } else {
+    return newArr;
+  }
+
 }
