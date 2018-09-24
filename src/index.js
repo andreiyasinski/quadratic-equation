7a72028be13de349290a4465803a1f0c38e32b6f
andreiyasinski <andrewtopol856@gmail.com>
2018-09-24 19:59:28 +0300

    :bulb:

--- src/index.js
+++ src/index.js
@@ -1,28 +1,23 @@
 module.exports = function solveEquation(equation) {
 
-  var compareFunc = (a,b) => a - b;
+  let compareFunc = (a,b) => a - b;
 
-  var x1 = 0;
-  var x2 = 0;
-
-  var equation = equation;
-
-  var arr = [];
-  var newArr = [];
+  let arr = [];
+  let newArr = [];
 
   arr = equation.split(' * ');
 
-  var a = +arr[0];
-  var [, simbol1, value1] = arr[1].split(' ');
-  var b = +(simbol1 + '' + value1);
-  var [, simbol2, value2] = arr[2].split(' ');
-  var c = +(simbol2 + '' + value2);
+  let a = +arr[0];
+  let [, simbol1, value1] = arr[1].split(' ');
+  let b = +(simbol1 + '' + value1);
+  let [, simbol2, value2] = arr[2].split(' ');
+  let c = +(simbol2 + '' + value2);
 
-  var d = b*b - 4*a*c;
+  let d = b*b - 4*a*c;
 
   if( d >= 0 ) {
-    x1 = Math.round((-b + Math.sqrt(d)) / (2 * a));
-    x2 = Math.round((-b - Math.sqrt(d)) / (2 * a));
+    let x1 = Math.round((-b + Math.sqrt(d)) / (2 * a));
+    let x2 = Math.round((-b - Math.sqrt(d)) / (2 * a));
 
     newArr.push(x1);
     newArr.push(x2);
